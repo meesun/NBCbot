ac71a9d15a484931d593ef536bd0b322082f2cc9
meesun <meesunsat@gmail.com>
2017-07-08 22:25:08 +0530

    Facebook code updates

--- app.js
+++ app.js
@@ -10,16 +10,6 @@ app.get('/', function (req, res) {
     res.send('hello world');
 });
 
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === 'poda dai') {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
-});
 
 app.listen(process.env.PORT || 3000 , function () {
     console.log('Example app listening on port 3000!');
