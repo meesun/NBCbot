3b8f9c6b1eb6a962eb7e21bd75eaf7df87e3d28e
meesun <meesunsat@gmail.com>
2017-07-08 21:43:17 +0530

    no message

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
     res.send('hello world');
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000 , function () {
     console.log('Example app listening on port 3000!');
 });
 
