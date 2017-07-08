e76da0950d83e8e35ed789224e8ddf920abdfa9a
meesun <meesunsat@gmail.com>
2017-07-08 22:35:30 +0530

    adding body parser

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var facebook = require(__dirname+'/routes/facebook')();
-
-
+var bodyParser = require('body-parser');
 
 var app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
 
 app.use('/facebook',facebook);
 app.get('/', function (req, res) {
