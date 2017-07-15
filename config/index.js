9b0729f854ac416a330ef16e4ee6715d73af2a9c
Sapna Jayavel <Sapna.Jayavel@siriuscom.com>
2017-07-15 15:20:25 +0530

    Mongo lab db

--- config/index.js
+++ config/index.js
@@ -3,6 +3,6 @@
  */
 module.exports = {
 	database: {
-		mlabs: "mongodb://nebulahcl:Saibaba@123@ds059516.mlab.com:59516/nebulahcl"
+		mlabs: "mongodb://nebula:nebula@ds059516.mlab.com:59516/nebulahcl"
     }
 };
