3f3eefb19652566e6061e723eb4687ba330e1f5f
Mani <manikanta.tankala@siriuscom.com>
2017-07-15 14:49:47 +0530

    code set up

--- app.js
+++ app.js
@@ -1,23 +1,69 @@
 var express = require('express');
-var facebook = require(__dirname+'/routes/facebook')();
 var bodyParser = require('body-parser');
+var constants = require('./modules/constants');
+var fbMessenger = require('./modules/fbMessenger');
 
 var app = express();
+
+app.set('port', (process.env.PORT || 8080));
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(express.static('WebContent'));
 
-
-
-app.use('/facebook',facebook);
 app.get('/', function (req, res) {
     res.send('hello world');
 });
 
-
-app.listen(process.env.PORT || 3000 , function () {
-    console.log('Example app listening on port 3000!');
+app.get('/webhook/', function(req, res) {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge']);
+        return;
+    }
+    res.send('Error, wrong token');
 });
 
+app.post('/webhook/', function(req, res) {
+    var data = req.body;
+
+    // Make sure this is a page subscription
+    if (data.object == 'page') {
+        // Iterate over each entry
+        // There may be multiple if batched
+        data.entry.forEach(function(pageEntry) {
+            var pageID = pageEntry.id;
+            var timeOfEvent = pageEntry.time;
+
+            // Iterate over each messaging event
+            pageEntry.messaging.forEach(function(messagingEvent) {
+                if (messagingEvent.optin) {
+                    fbMessenger.receivedAuthentication(messagingEvent);
+                } else if (messagingEvent.message) {
+                    fbMessenger.receivedMessage(messagingEvent);
+                } else if (messagingEvent.delivery) {
+                    fbMessenger.receivedDeliveryConfirmation(messagingEvent);
+                } else if (messagingEvent.postback) {
+                    fbMessenger.receivedPostback(messagingEvent);
+                } else if (messagingEvent.read) {
+                    fbMessenger.receivedMessageRead(messagingEvent);
+                } else if (messagingEvent.account_linking) {
+                    fbMessenger.receivedAccountLink(messagingEvent);
+                } else {
+                    console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+                }
+            });
+        });
+
+        // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds, to let us know you've
+        // successfully received the callback. Otherwise, the request will time out.
+        res.sendStatus(200);
+    }
+})
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+});
