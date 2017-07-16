adbf351bb7aac34fe48a8b877a2233c47bb98764
Sapna Jayavel <Sapna.Jayavel@siriuscom.com>
2017-07-16 08:36:12 +0530

    User data

--- routes/users.js
+++ routes/users.js
@@ -13,3 +13,37 @@ module.exports = function() {
 	return router;
 
 }
+
+
+
+/*
+ * Send a read receipt to indicate the message has been read
+ *
+ */
+function saveUserProfileDate(senderId, senderData) {
+    console.log("SAving the user data: " + senderId + ":=" + senderData);
+
+
+    var Users = require(__base + 'models/users');
+
+	var users = Users({
+		id: '100',
+			ques: 'Who acted in Bahubali?',
+			option: [
+				{'optId':'A','optVal':'Shahrukh'},
+				{'optId':'B','optVal':'Pawan Kalyan'},
+				{'optId':'C','optVal':'Prabhas'},
+				{'optId':'D','optVal':'Rajesh'}
+			],
+			answer: 'C'
+	});
+
+	users.save(function(err) {
+		if (err) next(err);
+
+		console.log('user created!');
+		res.send('user created!');
+	});
+}
+
+module.saveUserProfileDate = saveUserProfileDate;
