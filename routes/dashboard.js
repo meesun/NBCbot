d8fd68e921e2d80e02dbc70f5dbdb07c8710bab4
meesun <meesunsat@gmail.com>
2017-07-16 20:49:34 +0530

    dashbot commit

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,26 +4,27 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function() {
+    //deprecated
     router.get('/getAns', function(req, res) {
 		dashboard.getAnswer(req,res);
 	});
-
+    //deprecated
 	router.get('/getQues', function(req, res, next) {
 		
         dashboard.getQuestionById(req,res);
             
         
 	});
-    
+    //deprecated
     router.get('/saveAns', function(req, res) {
 		dashboard.saveAnswer(req,res);
 	});
-
+    //deprecated
 	router.get('/saveQues', function(req, res, next) {
 		
         dashboard.saveQuestion(req,res);
 	});
-    
+    //deprecated
     router.get('/getQnAnsByUser', function(req,res){
         dashboard.getQnAnsByUser(req,res);
     });
@@ -41,6 +42,9 @@ module.exports = function() {
     });
     router.get('/getTagsfromReviews', function(req,res){
         dashboard.getTagsfromReviews(req,res);
+    });
+    router.get('/getAllQuestions', function(req,res){
+        dashboard.getAllQuestions(req,res);
     });
     
     
