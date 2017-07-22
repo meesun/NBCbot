d3047f7062929da4aa51fb51a140ac35d3559348
Mani <manikanta.tankala@siriuscom.com>
2017-07-22 23:05:43 +0530

    code format

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,63 +5,64 @@ var q = require('q');
 var router = express.Router();
 
 module.exports = function() {
-    
-    router.get('/getAns', function(req, res) {
-		dashboard.getAnswer(req,res);
-	});
-    //deprecated
-	router.get('/getQues', function(req, res, next) {
-		
-        dashboard.getQuestionById(req,res);
-            
-        
-	});
-    //deprecated
-    router.get('/saveAns', function(req, res) {
-		dashboard.saveAnswer(req,res);
-	});
-    //deprecated
-	router.get('/saveQues', function(req, res, next) {
-		
-        dashboard.saveQuestion(req,res);
-	});
-    //deprecated
-    router.get('/getQnAnsByUser', function(req,res){
-        dashboard.getQnAnsByUser(req,res);
-    });
-    
-    router.get('/getShows', function(req,res){
-        dashboard.getShows(req,res);
-    });
-    
-     router.get('/getShowReviews', function(req,res){
-        dashboard.getShowReviews(req,res);
-    });
-    
-    router.get('/getShowDataByCountry', function(req,res){
-        dashboard.getgeographicaldataByShow(req,res);
-    });
-    router.get('/getTagsfromReviews', function(req,res){
-        dashboard.getTagsfromReviews(req,res);
-    });
-    router.get('/getAllQuestions', function(req,res){
-        dashboard.getAllQuestions(req,res);
-    });
-    
-    router.get('/getAgeDataByShow', function(req,res){
-        dashboard.getAgeDataByShow(req,res);
-    });
-    
-    router.get('/saveShows', function(req,res){
-        dashboard.saveShows(req,res);
-    });
-    router.get('/getQuizQuestions', function(req,res){
-        dashboard.getQuizQuestions(req,res);
-    });
-    
-    
-    
-    return router;
+
+  router.get('/getAns', function(req, res) {
+    dashboard.getAnswer(req, res);
+  });
+
+  //deprecated
+  router.get('/getQues', function(req, res, next) {
+    dashboard.getQuestionById(req, res);
+  });
+
+  //deprecated
+  router.get('/saveAns', function(req, res) {
+    dashboard.saveAnswer(req, res);
+  });
+
+  //deprecated
+  router.get('/saveQues', function(req, res, next) {
+    dashboard.saveQuestion(req, res);
+  });
+  
+  //deprecated
+  router.get('/getQnAnsByUser', function(req, res) {
+    dashboard.getQnAnsByUser(req, res);
+  });
+
+  router.get('/getShows', function(req, res) {
+    dashboard.getShows(req, res);
+  });
+
+  router.get('/getShowReviews', function(req, res) {
+    dashboard.getShowReviews(req, res);
+  });
+
+  router.get('/getShowDataByCountry', function(req, res) {
+    dashboard.getgeographicaldataByShow(req, res);
+  });
+
+  router.get('/getTagsfromReviews', function(req, res) {
+    dashboard.getTagsfromReviews(req, res);
+  });
+
+  router.get('/getAllQuestions', function(req, res) {
+    dashboard.getAllQuestions(req, res);
+  });
+
+  router.get('/getAgeDataByShow', function(req, res) {
+    dashboard.getAgeDataByShow(req, res);
+  });
+
+  router.get('/saveShows', function(req, res) {
+    dashboard.saveShows(req, res);
+  });
+
+  router.get('/getQuizQuestions', function(req, res) {
+    dashboard.getQuizQuestions(req, res);
+  });
+
+  return router;
 }
 
 
@@ -71,28 +72,38 @@ module.exports = function() {
  */
 function getFeedbackQuestionList(showName) {
 
-	var Qnas = require(__base + 'models/qna');
-	var deferred = q.defer();
-	Qnas.find({show:showName,type:"feedback"}, function(err, qnas) {
-		if (err) console.log(err);
-		console.log(qnas);
-		deferred.resolve(qnas);
-	});
-	return deferred.promise;
-    
+  var Qnas = require(__base + 'models/qna');
+  var deferred = q.defer();
+  Qnas.find({
+    show: showName,
+    type: "feedback"
+  }, function(err, qnas) {
+    if (err) console.log(err);
+    console.log(qnas);
+    deferred.resolve(qnas);
+  });
+  return deferred.promise;
+
 }
 
 
 
-function setUserIdInQuestion(senderId,quesId){
+function setUserIdInQuestion(senderId, quesId) {
 
-	var Qnas = require(__base + 'models/qna');
-      Qnas.findOneAndUpdate({_id:quesId},
-       {$push: {"userId": senderId}},
-       {safe: true, upsert: true, new : true}, 
-       function (err, place) {
-      });
+  var Qnas = require(__base + 'models/qna');
+  Qnas.findOneAndUpdate({
+      _id: quesId
+    }, {
+      $push: {
+        "userId": senderId
+      }
+    }, {
+      safe: true,
+      upsert: true,
+      new: true
+    },
+    function(err, place) {});
 }
 
-module.exports.getFeedbackQuestionList=getFeedbackQuestionList;
-module.exports.setUserIdInQuestion=setUserIdInQuestion;
+module.exports.getFeedbackQuestionList = getFeedbackQuestionList;
+module.exports.setUserIdInQuestion = setUserIdInQuestion;
