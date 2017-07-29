0bdc578ae3caa95d68ba57c7fd8aecd68c46beea
aninja8 <jananib@JananiMacBook.local>
2017-07-29 17:07:16 +0530

    Trending shows

--- modules/fbMessenger.js
+++ modules/fbMessenger.js
@@ -1109,9 +1109,10 @@ function callSendAPI(messageData) {
         var elements = [];
         if(response.length>0){
           console.log("looping through")
+          var elements=[];
+
         for (i = 0; i < response.length; i++) {
               var show= response[i];
-              var elements=[];
               var showElement={
                 title: show.name,
                 subtitle:show.description,
